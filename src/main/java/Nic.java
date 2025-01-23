03309370c02d5aab871f90f41cb426a3cee7501b
nictzl <nicholastanzhiloong@gmail.com>
2025-01-23 19:48:17 +0800

    Created my own Nic chatbot

---
+++ src/main/java/Nic.java
@@ -0,0 +1,8 @@
+public class Nic {
+    public static void main(String[] args) {
+        String entry = "Hello! I'm Nic ";
+        String welcome = "How can I help you today?";
+        System.out.println(entry);
+        System.out.println(welcome);
+   }
+}
