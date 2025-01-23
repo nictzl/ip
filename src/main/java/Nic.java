b9eca4335b3be503a0434d271c31ace33c79b6db
nictzl <nicholastanzhiloong@gmail.com>
2025-01-23 19:51:28 +0800

    no message

--- src/main/java/Nic.java
+++ src/main/java/Nic.java
@@ -2,7 +2,9 @@ public class Nic {
     public static void main(String[] args) {
         String entry = "Hello! I'm Nic ";
         String welcome = "How can I help you today?";
+        String exit = "Hope I was able to help you, goodbye";
         System.out.println(entry);
         System.out.println(welcome);
+        System.out.println(exit);
    }
 }
